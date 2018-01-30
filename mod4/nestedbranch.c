52cd8d37ec4bace33ad36dd787c6e09b7a160f28
Bo Christensen <BowmanChristensen@mail.weber.edu>
2018-01-30 10:15:00 -0700

    added nested

---
+++ mod4/nestedbranch.c
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  nestedbranch.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/30/2018 10:10:53 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Bo Christensen (), BowmanChristensen@mail.weber.edu
+ *   Organization:  wsu
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int income = 30000;
+    if(income)
+    {
+        printf("Income is %d\n", income);
+    }
+    else
+    {
+        printf("No income\n");
+    }
+    return 0;
+}
+// Function Definitions
+
+
