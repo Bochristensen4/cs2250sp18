02a8a58e2d7dcdc8a69dd0dcbf8d2be3728cf60d
Bo Christensen <BowmanChristensen@mail.weber.edu>
2018-02-27 08:46:45 -0700

    Initial Commit

---
+++ mid1/Bo_Christensen_Exam_1_task1.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Bo_Christensen_Exam_1_task1.c
+ *
+ *    Description:  fist choice
+ *
+ *        Version:  1.0
+ *        Created:  02/27/2018 08:42:38 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Bo Christensen (), BowmanChristensen@mail.weber.edu
+ *   Organization:  wsu
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
